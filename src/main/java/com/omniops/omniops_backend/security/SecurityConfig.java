eb5f7b2ad7089daca3d8017950c35629c30f03f7
Usha Madithati <ushamadithati712@gmail.com>
2026-06-30 21:58:55 +0530

    Allow mrrecosys.com CORS#

--- src/main/java/com/omniops/omniops_backend/security/SecurityConfig.java
+++ src/main/java/com/omniops/omniops_backend/security/SecurityConfig.java
@@ -55,14 +55,14 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(
-                List.of(
-                        "http://127.0.0.1:5500",
-                        "http://localhost:5500"
-                )
-        );
-
+configuration.setAllowedOrigins(
+        List.of(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "https://mrrecosys.com",
+                "https://www.mrrecosys.com"
+        )
+);
         configuration.setAllowedMethods(
                 List.of("GET","POST","PUT","DELETE","OPTIONS")
         );
